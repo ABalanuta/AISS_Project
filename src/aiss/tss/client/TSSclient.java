dfcc8994754e1355cab969c57dc9106d27b11de0
Artur Balanuta <artur.balanuta@ist.utl.pt>
2013-04-17 20:46:32 +0100

    tsp

--- src/aiss/tss/client/TSSclient.java
+++ src/aiss/tss/client/TSSclient.java
@@ -2,4 +2,14 @@ package aiss.tss.client;
 
 public class TSSclient {
 
+	
+	
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello Humans I am A Client");
+		
+	}
+	
+	
 }
--- src/aiss/tss/server/TSSserver.java
+++ src/aiss/tss/server/TSSserver.java
@@ -1,5 +1,22 @@
 package aiss.tss.server;
 
+
+import org.bouncycastle.tsp.TSPUtil;
+
+
 public class TSSserver {
 
+	private TSPUtil tsp = new TSPUtil();
+	
+	
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello Humans I am A Server");
+		
+		
+		
+		
+	}
+	
 }
