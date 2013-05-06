b96d1530887a228f680f12bf1385b34e8a3e8451
Artur Balanuta <artur.balanuta@ist.utl.pt>
2013-05-06 02:50:41 +0100

    Partial(Broken): ENCRYPTION feita com software e hardware, Benny vai fazer Descifra e testar a parte do JNI em Mac

--- plugin/chrome/content/java/Script.sh
+++ plugin/chrome/content/java/Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash          
 SCRIPT=$(dirname $(readlink -f $0))
 cd $SCRIPT
-java -jar '-Djava.library.path=/usr/local/lib/pteid_jni/:.' "AESecure.jar" $1  > "log.txt"
+java -jar '-Djava.library.path=/usr/local/lib/pteid_jni/' "AESecure.jar" $1  > "log.txt"
 #rm input.zip
 cat log.txt
--- src/aiss/main/EncryptionEngine.java
+++ src/aiss/main/EncryptionEngine.java
@@ -17,6 +17,7 @@ import javax.crypto.Cipher;
 import javax.security.cert.CertificateException;
 import javax.security.cert.X509Certificate;
 
+import aiss.aesBox.AESboxJNI;
 import aiss.aesJava.MyAES;
 import aiss.tss.client.TSSClient;
 
@@ -29,6 +30,7 @@ import sun.security.pkcs11.wrapper.PKCS11Exception;
 public class EncryptionEngine implements Engine{
 
 	private static final boolean DEBUG = true;
+	private static final boolean AES_BOX_PRESENT = true;
 	private Boolean authentication = null;
 	private Boolean confidentiality = null;
 	private Boolean timestamping = null;
@@ -55,17 +57,20 @@ public class EncryptionEngine implements Engine{
 	@Override
 	public void run() {
 		debug("Engine Run");
-		String contentsBase64 = "";
-		String signatureBase64 = "";
-		String certBase64 = "";
-		String timeStampSignBase64 = "";
-
+		
 		FileManager fm = new FileManager();
 		BASE64Encoder base64encoder = new BASE64Encoder();
-
-		byte[] fileByteContent = fm.getFolderContentInZipByteArray();
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-
+		
+		byte[] fileByteContent = null;
+		byte[] cert = null;
+		byte[] signature = null;
+		byte[] timeStamp = null;
+		
+		
+		// Adquire os conteudos da pacsta
+		fileByteContent = fm.getFolderContentInZipByteArray();
+		
 		// If no file Found return 
 		if(fileByteContent == null){
 			return;
@@ -77,7 +82,7 @@ public class EncryptionEngine implements Engine{
 			debug("Engine Authentication Service");
 
 			try {
-				certBase64 = base64encoder.encode(aiss.ccauthentication.Signature.obtainCert());
+				cert = aiss.ccauthentication.Signature.obtainCert();
 
 				//first hash/digest
 				MessageDigest md1 = MessageDigest.getInstance("SHA-256");
@@ -93,40 +98,79 @@ public class EncryptionEngine implements Engine{
 				System.arraycopy(mdigest1,0,mdigestTotal,0,mdigest1.length);
 				System.arraycopy(mdigest2,0,mdigestTotal,mdigest1.length,mdigest2.length);
 
-				signatureBase64 = base64encoder.encode(aiss.ccauthentication.Signature.createSignature(mdigestTotal));
+				signature = aiss.ccauthentication.Signature.createSignature(mdigestTotal);
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
+		
+		
 		if(timestamping){
 			debug("Engine TimeStamping Service");
-
-			BASE64Encoder encoder = new BASE64Encoder();
+			
 			byte[] textHash = TSSClient.byteDigestSHA256(fileByteContent);
-			debug("Text SHA-256 Hash: " + encoder.encode(textHash));
+			debug("Text SHA-256 Hash: " + base64encoder.encode(textHash));
 
-			byte[] timeStamp = TSSClient.generateTimeStamp(textHash);
-			timeStampSignBase64 = encoder.encode(timeStamp);
+			timeStamp = TSSClient.generateTimeStamp(textHash);
 		}
 
+		
+		
+		//Cifrar todos os Campos a enviar
 		if(confidentiality){
 			debug("Engine Confidentiality Service");
-
-			// BACKUP MODE
-			MyAES aes = new MyAES();
-			fileByteContent = aes.CipherAll(MyAES.ENCRYPT, "AES_KEY_256.key", MyAES.CBC, fileByteContent);
 			
+			fileByteContent = Cipher(fileByteContent);
+			
+			if(authentication){
+				cert = Cipher(cert);
+				signature = Cipher(signature);
+			}
+			
+			if(timestamping){
+				timeStamp = Cipher(timeStamp);
+			}	
 		}
 
-		// Passar o conteudo para Base64
-		contentsBase64 = base64encoder.encode(fileByteContent);
-
+		
+		// Passar os campos para Base64
+		String contentsBase64 = base64encoder.encode(fileByteContent);
+		String certBase64 = "";
+		String signatureBase64 = "";
+		String timeStampSignBase64 = "";
+		
+		if(authentication){
+			certBase64 = base64encoder.encode(cert);
+			signatureBase64 = base64encoder.encode(signature);
+		}
+		
+		if(timestamping){
+			timeStampSignBase64 = base64encoder.encode(timeStamp);
+		}
+		
 		// Escrever o conteudo para um ficheiro XML
 		fm.writeXML(operations, contentsBase64, certBase64, signatureBase64,timeStampSignBase64);
 	}
-
+	
+	// Classe que escolha um dos dois Devices da Cifra AES, utilizando a box
+	// ou utilizando o modulo de software criado no laboratorio
+	private byte[] Cipher(byte[] content){
+		
+		if(AES_BOX_PRESENT){
+			debug("Using HARDWARE for AES Encryption");
+			AESboxJNI aes = new AESboxJNI();
+			return aes.CipherAll(AESboxJNI.ENCRYPT, content);
+		}else{
+			debug("Using JAVA for AES Encryption");
+			// BACKUP MODE
+			MyAES aes = new MyAES();
+			return aes.CipherAll(MyAES.ENCRYPT, "AES_KEY_256.key", MyAES.CBC, content);
+		}
+	}
+	
+	
 	private static void debug(String log){
 		if(DEBUG){
 			System.out.println(log);
