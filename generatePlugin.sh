1e28eb8fd19ea12a5666b50a0cd7b75a69471a9a
Bernardo Santos <bernardo.mpsantos@gmail.com>
2013-05-10 21:30:13 +0100

    - script updated!

--- generatePlugin.sh
+++ generatePlugin.sh
@@ -11,16 +11,16 @@ if [ -z "$1" ]; then
 	exit
 fi
 
-
-rm CustomPlugin -R
+rm -r CustomPlugin
 mkdir CustomPlugin
-cp plugin/* -R CustomPlugin/
+cp -r plugin/* CustomPlugin/
 cd src/aiss/aesBox/
 make clean
 sh compile.sh
 cp libaesbox.* ../../../CustomPlugin/chrome/content/java/
 cd ../../../CustomPlugin/
 cd chrome/content/java/
+
 if [ "$1"="-box" ]; then
 	cp ScriptBoxAES.sh Script.sh
 	cp ScriptMACboxAES.sh ScriptMAC.sh
@@ -32,8 +32,8 @@ if [ "$1"="-java" ]; then
 fi
 cd ../../../
 zip -r AESecure.xpi * -x AESecure.xpi -9
-rm chrome -R
-rm modules -R
+rm -r chrome
+rm -r modules
 rm chrome.manifest install.rdf
 clear
 echo ###################################
