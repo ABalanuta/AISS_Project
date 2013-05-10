63a1b94082e717ba9a3dc9540cf29a2be0b81304
Artur Balanuta <artur.balanuta@ist.utl.pt>
2013-05-10 07:19:22 +0100

    up

--- plugin/chrome/content/java/Script.sh
+++ plugin/chrome/content/java/Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash          
 SCRIPT=$(dirname $(readlink -f $0))
-BOX=1
+BOX=0
 cd $SCRIPT
 java -jar '-Djava.library.path=/usr/local/lib/pteid_jni/' "AESecure.jar" $1 $BOX > "log.txt"
 #rm input.zip
